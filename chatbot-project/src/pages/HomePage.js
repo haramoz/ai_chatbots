64b378f9e297471b92367f89c31d66702f45d148
arka <arkamallick30@gmail.com>
2023-06-10 08:12:38 +0200

    wip chatbot integration

--- chatbot-project/src/pages/HomePage.js
+++ chatbot-project/src/pages/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import ChatAssistant from '../components/Chatbot'
 
 const HomePage = () => {
   return (
     <div>
       <h1>Welcome to the HomePage!</h1>
       <p>This is the default home page of our React application.</p>
+      <ChatAssistant/>
     </div>
   );
 };
